3336fb4887968a96f9c3ff05526ca976e7c6f323
Poliinaa <50701462+Poliinaa@users.noreply.github.com>
2019-05-18 12:02:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-// forPolina.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include "pch.h"
 #include <iostream>
 #include <iomanip>
@@ -83,6 +80,7 @@ public:
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
